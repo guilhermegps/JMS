fae3263acb27e474bfa81d19ad2dcd16d46df6fb
Guilherme <guilherme.santos@eigmercados.com.br>
2019-02-14 17:55:30 -0200

    Consumindo a fila Dead Letter Queue

--- src/br/com/teste/jms/TesteConsumidorFilaDLQ.java
+++ src/br/com/teste/jms/TesteConsumidorFilaDLQ.java
@@ -1,19 +1,20 @@
 package br.com.teste.jms;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageProducer;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import br.com.teste.modelo.Pedido;
-import br.com.teste.modelo.PedidoFactory;
-
-public class TesteProdutorTopico {
+public class TesteConsumidorFilaDLQ {
 	public static void main(String[] args) throws NamingException, JMSException {
 		
 		InitialContext context = new InitialContext();
@@ -23,17 +24,20 @@ public class TesteProdutorTopico {
 		connection.start();
 		
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Destination topico = (Destination) context.lookup("loja");
+		Destination fila = (Destination) context.lookup("DLQ");
+		MessageConsumer consumer = session.createConsumer(fila);
 		
-		MessageProducer producer = session.createProducer(topico);
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				
+				System.out.println(message);
+			}
+		});
 		
-		Pedido pedido = PedidoFactory.geraPedidoComValores();
-		
-//		StringWriter xml = new StringWriter();
-//		JAXB.marshal(pedido, xml);		
-		Message message = session.createObjectMessage(pedido);
-//			message.setBooleanProperty("ebook", false);
-		producer.send(message);
+				
+		new Scanner(System.in).next();
 		
 		session.close();
 		connection.close();
